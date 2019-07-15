0046e3aea5d9e59fd4506e3c6fc2ffb9abc8ab45
a21802 <a21802nec@gmail.com>
2019-07-15 16:30:54 +0800

    update

---
+++ C++/image processing/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(int arg, char** args) {
+	
+    cout << "Ok";
+    system("pause");
+
+    return 0;
+}
---
+++ C++/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello" << endl;
+    system("pause");
+    return 0;
+}
